5ff79cde09b730417c83b6e22fc9070d45bf82c2
miya-algoma <miya@algomau.ca>
2025-07-13 15:47:24 +0000

    Finally fixed Reports and it was because it was a capital R omg haha

--- app/core/App.php
+++ app/core/App.php
@@ -8,27 +8,31 @@ class App {
     protected $params = [];
 
     public function __construct() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
         if (isset($_SESSION['auth']) && $_SESSION['auth'] === 1) {
             $this->controller = 'home';
         }
 
         $url = $this->parseUrl();
 
-        
-        if (isset($url[1]) && file_exists('app/controllers/' . $url[1] . '.php')) {
-            $this->controller = $url[1];
-            $_SESSION['controller'] = $this->controller;
+        if (isset($url[1])) {
+            $controllerName = ucfirst(strtolower($url[1]));
+            if (file_exists('app/controllers/' . $controllerName . '.php')) {
+                $this->controller = $controllerName;
+                $_SESSION['controller'] = $this->controller;
 
-            if (in_array($this->controller, $this->special_url)) {
-                $this->method = 'index';
+                if (in_array(strtolower($url[1]), $this->special_url)) {
+                    $this->method = 'index';
+                }
+
+                unset($url[1]);
+            } else {
+                header('Location: /home');
+                exit;
             }
-
-            unset($url[1]);
-        } elseif (!isset($url[1]) || empty($url[1])) {
-            // Allow root to load default controller/method
-        } else {
-            header('Location: /home');
-            exit;
         }
 
         require_once 'app/controllers/' . $this->controller . '.php';
@@ -42,7 +46,7 @@ class App {
         }
 
         $this->params = $url ? array_values($url) : [];
-        call_user_func_array([$this->controller, $this->method], $this->params);		
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseUrl() {
