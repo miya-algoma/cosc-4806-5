9ba49262fc5c5723d98bd078f254d630012695b3
miya-algoma <miya@algomau.ca>
2025-07-13 19:35:49 +0000

    Public header and footer

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -1,17 +1,19 @@
 <?php
-if (isset($_SESSION['auth']) == 1) {
+if (session_status() === PHP_SESSION_NONE) session_start();
+if (isset($_SESSION['auth'])) {
     header('Location: /home');
+    exit;
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel="icon" href="/favicon.png">
     <title>COSC 4806</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="mobile-web-app-capable" content="yes" />
 </head>
 <body>
