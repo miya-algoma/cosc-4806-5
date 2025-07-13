6b923a2fce25d7ebc3f9d08cf6958686c5eda9fd
miya-algoma <miya@algomau.ca>
2025-07-13 16:45:17 +0000

    Completed having issues

--- app/core/App.php
+++ app/core/App.php
@@ -7,61 +7,36 @@ protected $method = 'index';
 protected $params = [];
 
 public function __construct() {
+if (session_status() === PHP_SESSION_NONE) session_start();
 
-if (session_status() === PHP_SESSION_NONE) {
-session_start();
+if (isset($_SESSION['auth']) && $_SESSION['auth'] === 1) {
+$this->controller = 'home';
 }
-
 
 $url = $this->parseUrl();
 
-
-if (isset($url[1]) && !empty($url[1])) {
-$controllerName = ucfirst(strtolower($url[1])); 
-$controllerPath = "app/controllers/{$controllerName}.php";
-
-if (file_exists($controllerPath)) {
-
-$this->controller = $controllerName;
+if (isset($url[1]) && file_exists('app/controllers/' . $url[1] . '.php')) {
+$this->controller = $url[1];
 unset($url[1]);
-} else {
-
-if (isset($_SESSION['auth']) && $_SESSION['auth'] == 1) {
-$this->controller = 'home';
-} else {
-$this->controller = 'login';
-}
-}
-} else {
-
-if (isset($_SESSION['auth']) && $_SESSION['auth'] == 1) {
-$this->controller = 'home';
-} else {
-$this->controller = 'login';
-}
 }
 
+require_once 'app/controllers/' . $this->controller . '.php';
 
-require_once "app/controllers/{$this->controller}.php";
 $this->controller = new $this->controller;
-
 
 if (isset($url[2]) && method_exists($this->controller, $url[2])) {
 $this->method = $url[2];
 unset($url[2]);
 }
 
-
 $this->params = $url ? array_values($url) : [];
-
 
 call_user_func_array([$this->controller, $this->method], $this->params);
 }
 
-private function parseUrl() {
-$u = $_SERVER['REQUEST_URI'];
-$url = explode('/', filter_var(rtrim($u, '/'), FILTER_SANITIZE_URL));
-unset($url[0]); 
-return $url;
+public function parseUrl() {
+$url = rtrim($_SERVER['REQUEST_URI'], '/');
+$url = filter_var($url, FILTER_SANITIZE_URL);
+return explode('/', $url);
 }
 }
